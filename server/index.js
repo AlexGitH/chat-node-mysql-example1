543cb76e2da7e58914749ee4433bfe5ffc10662a
Alex Bern <pas801012@gmail.com>
2021-08-03 14:57:59 +0300

    Fix path error for build directory;

--- server/index.js
+++ server/index.js
@@ -67,7 +67,7 @@ if ( MODE === 'PRODUCTION' ) {
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
 
